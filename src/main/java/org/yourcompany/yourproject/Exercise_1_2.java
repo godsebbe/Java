d83e73935e614a42bc91ef7da7c9b4ce17d1bbf5
godsebbe <61354727+godsebbe@users.noreply.github.com>
2025-01-19 18:43:26 -0500

    1.2

---
+++ src/main/java/org/yourcompany/yourproject/Exercise_1_2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+ package org.yourcompany.yourproject;
+
+
+
+ /**
+  *
+  * @author sebas
+  */
+ public class Exercise_1_2 {
+ 
+     public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+        System.out.println("Welcome to Java");
+        
+        }
+     }
+ }
+ 
