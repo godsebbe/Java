3dc71720092fcae84247cf7beedbd6ab98779f02
godsebbe <61354727+godsebbe@users.noreply.github.com>
2025-01-19 18:18:15 -0500

    1.1

---
+++ src/main/java/org/yourcompany/yourproject/Java.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.yourcompany.yourproject;
+
+/**
+ *
+ * @author sebas
+ */
+public class Java {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!f");
+    }
+}
