60db239105b6d82594811a2b45ed6442815e3864
godsebbe <61354727+godsebbe@users.noreply.github.com>
2025-02-28 21:10:03 -0500

    update to exercise 4.4

--- src/main/java/org/yourcompany/yourproject/Exercise_4_4.java
+++ src/main/java/org/yourcompany/yourproject/Exercise_4_4.java
@@ -5,13 +5,13 @@ public class Exercise_4_4 {
 
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in); // scanner
 
-        System.out.print("Enter the side: ");
+        System.out.print("Enter the side: "); // input prompt
         double s = input.nextDouble();
-        double area = (6 * Math.pow(s, 2)) / (4 * Math.tan(Math.PI / 6));
-        System.out.printf("The are of the hexagon is %.2f\n", area);
-        input.close();
+        double area = (6 * Math.pow(s, 2)) / (4 * Math.tan(Math.PI / 6)); // area calculation
+        System.out.printf("The are of the hexagon is %.2f\n", area); //printed message
+        input.close(); //closing the scanner.
     }
     
 }
